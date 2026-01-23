66d79787089bace251694dac5c91c17a4d553ca8
feri muhibudin <38092606+ferimuhibudin@users.noreply.github.com>
2026-01-24 03:10:24 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# DESC: untuk login ke server
+
+### HELP START ###
+show_help() {
+  cat << 'EOF'
+Cara pakai
+ ~/login.sh namavps
+
+Description:
+  Login ke container LXC sebagai user admin.
+
+Options:
+  -h, --help    Tampilkan help ini
+EOF
+}
+### HELP END ###
+
 
 if [ -z "$1" ]; then
   echo "Usage: $0 <nama-container>"
