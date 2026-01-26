579ce4128ccc0f2a222ae9b551bf0e5cb365b3e3
feri muhibudin <38092606+ferimuhibudin@users.noreply.github.com>
2026-01-27 00:45:28 +0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# DESC: untuk login ke server
-
-#!/bin/bash
-# DESC: login ke server LXC
-
-
-CONTAINER="$1"
-USER="admin"
-
-lxc exec -t "$CONTAINER" -- su - "$USER"
-
-
-
+# DESC: login ke server LXD
 
 if [ -z "$1" ]; then
   echo "Usage: $0 <nama-container>"
