84757477668f29f88d54580b41d19ad5d62e1afc
feri muhibudin <38092606+ferimuhibudin@users.noreply.github.com>
2026-02-17 08:22:52 +0700

    Update and rename export-show.sh to export-expose.sh

--- export-expose.sh
+++ export-expose.sh
@@ -3,7 +3,7 @@
 PORT=18001
 FOLDER="$HOME/pindah"
 
-# Ambil IPv4 public saja (paksa -4)
+# Ambil IPv4 public saja
 PUBLIC_IP=$(curl -4 -s https://ifconfig.me 2>/dev/null)
 
 # Fallback ke IPv4 lokal kalau tidak ada internet
@@ -13,9 +13,6 @@ fi
 
 echo "--------------------------------------------"
 echo "Folder $FOLDER telah di expose ke public."
-echo "Silakan akses di browser:"
-echo "http://$PUBLIC_IP:$PORT"
-echo ""
 echo "Tekan CTRL + C untuk menghentikan proses ini"
 echo "--------------------------------------------"
 echo ""
@@ -25,4 +22,4 @@ cd "$FOLDER" || {
     exit 1
 }
 
-python3 -m http.server $PORT --bind 0.0.0.0
+python3 -m http.server $PORT --bind "$PUBLIC_IP"
