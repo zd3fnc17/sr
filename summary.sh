c11b21d8d23e6590e02b1e9b09d37cc43607259c
feri muhibudin <38092606+ferimuhib@users.noreply.github.com>
2026-05-14 08:40:10 +0700

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -77,7 +77,7 @@ done
 
 cat <<'EOF'
 - jangan membuat paket melebihi batas
-- script versi 2.2
+- script versi 9.0
 - digiOS 9. rilis pada 14 mei 2026. memperbaiki webview pada meta trader 5
 ----
 EOF
